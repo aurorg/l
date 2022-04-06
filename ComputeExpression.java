202e7f1c59d2f55fd0959666afe50319d163bc62
aurorg <93782149+aurorg@users.noreply.github.com>
2022-04-06 22:43:46 +0800

    Add files via upload

---
+++ ComputeExpression.java
@@ -0,0 +1,17 @@
+/*public class ComputeExpression {
+    public static void main(String [] args){
+        System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+}
+ */
+//print 方法与println方法相同
+//println方法会在显示完一个字符串之后移到下一行的开始处
+//print方法在显示完成后不会进入下一行
+
+public class ComputeExpression {
+    public static void main(String[] args) {
+        System.out.print("(10.5+2*3) / (45-3.5) = ");
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+}
