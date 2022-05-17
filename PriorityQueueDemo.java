086801766396af58c5dc3dc154d716b83ee6b25c
aurorg <93782149+aurorg@users.noreply.github.com>
2022-05-17 23:31:00 +0800

    Add files via upload

---
+++ PriorityQueueDemo.java
@@ -0,0 +1,15 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+    public static void main(String[] args){
+        PriorityQueue<String> queue1 =new PriorityQueue<>();
+        queue1.offer("happy");
+        queue1.offer("luck");
+        queue1.offer("life");
+        queue1.offer("aurora");
+        System.out.println("Priority queue using Comparable: ");
+        while (queue1.size() >0){
+            
+        }
+    }
+}
