f7a960a837bd76d0765439f697de2acd6922631a
aurorg <93782149+aurorg@users.noreply.github.com>
2022-05-24 21:42:38 +0800

    Add files via upload

---
+++ Test_23_1.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Test_23_1 {
+    public static void main(String[] args){
+        int[] ints =insertionSort( new int[]{2,6,8,22,11,66});
+        System.out.println(Arrays.toString(ints));
+    }
+    public static int[] insertionSort(int[] array){
+        for(int i=1;i<array.length;i++){
+            for(int j=i-1;j>=0;j--){
+                if(array[j]>array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] =temp;
+                }
+            }
+        }
+        return array;
+    }
+
+}
+
